d2357519f0c06339769168a779cfa049fd8155b4
marcon78 <marcon78@sina.com>
2017-12-18 18:59:48 +0800

    2017-12-18 18:59

--- app/controllers/main/views.py
+++ app/controllers/main/views.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+import time
 from sqlalchemy import create_engine, select, func, String, Integer, Table, Column, MetaData
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask import request, render_template, current_app, jsonify
+from flask import request, render_template, current_app, jsonify, make_response, send_file
 from app import db
 
 from . import main
@@ -115,3 +116,198 @@ def data():
     # 直接返回json.dumps的结果是可行的，因为flask会判断并使用make_response方法自动构造出响应，只不过响应头各个字段是默认的。
     # 若要自定义响应字段，则可以使用make_response或Response自行构造响应。
     return jsonify(back_dt)
+
+
+from openpyxl.styles import Border, Side, Color, Fill, PatternFill, GradientFill, Font, Alignment
+def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment=None):
+    """
+    Apply styles to a range of cells as if they were a single cell.
+
+    :param ws:  Excel worksheet instance
+    :param range: An excel range to style (e.g. A1:F20)
+    :param border: An openpyxl Border
+    :param fill: An openpyxl PatternFill or GradientFill
+    :param font: An openpyxl Font object
+    """
+
+    top = Border(top=border.top)
+    left = Border(left=border.left)
+    right = Border(right=border.right)
+    bottom = Border(bottom=border.bottom)
+
+    first_cell = ws[cell_range.split(":")[0]]
+    if alignment:
+        ws.merge_cells(cell_range)
+        first_cell.alignment = alignment
+
+    rows = ws[cell_range]
+    if font:
+        first_cell.font = font
+
+    for cell in rows[0]:
+        cell.border = cell.border + top
+    for cell in rows[-1]:
+        cell.border = cell.border + bottom
+
+    for row in rows:
+        l = row[0]
+        r = row[-1]
+        l.border = l.border + left
+        r.border = r.border + right
+        if fill:
+            for c in row:
+                c.fill = fill
+
+@main.route("/export1", methods=["GET"])
+def export1():
+    t0 = time.clock()
+
+    # https://bitbucket.org/openpyxl/openpyxl/src
+    from openpyxl import Workbook
+    from openpyxl.writer.excel import save_virtual_workbook
+    # # from openpyxl.writer.excel import ExcelWriter
+    # from openpyxl.styles import Color, Fill, PatternFill, GradientFill, Font, Alignment
+    # # from openpyxl.cell import Cell
+    # # from openpyxl.utils import get_column_letter
+
+    # 新建一个workbook
+    wb = Workbook(write_only=True)
+    # 第一个sheet是ws
+    ws = wb.create_sheet()
+    # 设置ws的名称
+    ws.title = "统计"
+    ws.append(["CommodityNo", "CommodityName", "Quantity", "RealQuantity", "Price", "MakeDate", "Maker"])
+    # thin = Side(border_style="thin", color="000000")
+    # double = Side(border_style="double", color="ff0000")
+    # border = Border(top=double, left=thin, right=thin, bottom=double)
+    # fill = PatternFill("solid", fgColor="DDDDDD")
+    # # fill2 = GradientFill(stop=("000000", "FFFFFF"))
+    # font = Font(b=True, color="FF0000")
+    # al = Alignment(horizontal="center", vertical="center")
+    # # write_only 时不允许进行子脚本操作，也就无法定义相关的单元样式。
+    # style_range(ws, 'B2:F4', border=border, fill=fill, font=font, alignment=al)
+
+    # # 给A1赋值
+    # ws['A1'].value = '%s' % ("跟随总数")
+    # # 给A2赋值
+    # # 先把数字转换成字母
+    # col = get_column_letter(1)
+    # # 赋值
+    # ws.cell('%s%s' % (col, 2)).value = '%s' % ("A2")
+    # # 字体修改样式
+    # ##颜色
+    # ws.cell('A2').style.font.color.index = Color.GREEN
+    # ##字体名称
+    # ws.cell('A2').style.font.name = 'Arial'
+    # ##字号
+    # ws.cell('A2').style.font.size = 8
+    # ##加粗
+    # ws.cell('A2').style.font.bold = True
+    # ##不知道干啥用的
+    # ws.cell('A2').style.alignment.wrap_text = True
+    # ##背景 好像不太好用 是个BUG
+    # ws.cell('A2').style.fill.fill_type = Fill.FILL_SOLID
+    # ws.cell('A2').style.fill.start_color.index = Color.DARKRED
+    # ##修改某一列宽度
+    # ws.column_dimensions["C"].width = 60.0
+    # ##增加一个表
+    # ws = wb.create_sheet()
+    # ws.title = u'结单统计'
+    # # # 保存生成xlsx
+    # # file_name = 'test.xlsx'
+    # # file_dir = '/home/x/'
+    # # dest_filename = '%s%s' % (file_dir, file_name)
+    # # ew = ExcelWriter(workbook=wb)
+    # ew = ExcelWriter(workbook=wb)
+    res = db.session.execute("SELECT * FROM tbl_purchaseinfo;").fetchall()
+    for r in res:
+        ws.append(list(r))
+
+    # wb.save("new_big_file.xlsx")
+    # ew = ExcelWriter(workbook=wb)
+    # response = make_response(send_file("..//new_big_file.xlsx"))
+    response = make_response(save_virtual_workbook(wb))
+    response.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    response.headers["Content-Disposition"] = "attachment; filename=new_big_file1.xlsx;"
+
+    t = time.clock() - t0
+    print(t)
+
+    return response
+
+
+@main.route("/export2", methods=["GET"])
+def export2():
+    t0 = time.clock()
+
+    # https://github.com/pyexcel/pyexcel
+    import pyexcel as pe
+
+    data = [["CommodityNo", "CommodityName", "Quantity", "RealQuantity", "Price", "MakeDate", "Maker"]]
+    res = db.session.execute("SELECT * FROM tbl_purchaseinfo;").fetchall()
+    for r in res:
+        data.append(list(r))
+    sheet = pe.Sheet(data)
+    response = make_response(sheet.xlsx)
+    response.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    response.headers["Content-Disposition"] = "attachment; filename=new_big_file2.xlsx;"
+
+    t = time.clock() - t0
+    print(t)
+
+    return response
+
+
+# quick
+@main.route("/export3", methods=["GET"])
+def export3():
+    t0 = time.clock()
+
+    # xlrd/xlwt 只对xls的格式处理的比较好。
+    # https://github.com/python-excel/xlwt
+    import xlwt
+    from io import BytesIO
+
+    wb = xlwt.Workbook()
+    wb.encoding ='gbk'
+    ws = wb.add_sheet('1')
+    titles = ["CommodityNo", "CommodityName", "Quantity", "RealQuantity", "Price", "MakeDate", "Maker"]
+    for i in range(len(titles)):
+        ws.write(0, i, titles[i])
+
+    res = db.session.execute("SELECT * FROM tbl_purchaseinfo;").fetchall()
+    row = 1
+    for r in res:
+        for i in range(len(titles)):
+            ws.write(row, i, list(r)[i])
+        row += 1
+
+    bio = BytesIO()
+    wb.save(bio)   #这点很重要，传给save函数的不是保存文件名，而是一个StringIO流
+    response = make_response(bio.getvalue())
+    response.headers["Content-type"] = "application/vnd.ms-excel"   # 指定返回的类型
+    # response.headers["Transfer-Encoding"] = "chunked"
+    response.headers["Content-Disposition"] = "attachment;filename=new_big_file3.xls"  # 设定用户浏览器显示的保存文件名
+
+    t = time.clock() - t0
+    print(t)
+    return response
+
+
+@main.route("/export4", methods=["GET"])
+def export4():
+    t0 = time.clock()
+
+    # https://github.com/kennethreitz/tablib
+    import tablib
+    titles = ["CommodityNo", "CommodityName", "Quantity", "RealQuantity", "Price", "MakeDate", "Maker"]
+    res = db.session.execute("SELECT * FROM tbl_purchaseinfo;").fetchall()
+    data = tablib.Dataset(*list(res), headers=titles)
+    response = make_response(data.xlsx)
+    response.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    response.headers["Content-Disposition"] = "attachment; filename=new_big_file2.xlsx;"
+
+    t = time.clock() - t0
+    print(t)
+
+    return response
